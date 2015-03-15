f68ed7a84045e3ca966b661130136214b85be864
Adam Zmenak <adam.zmenak@gmail.com>
2015-03-15 16:40:08 +0800

    Make table sortable

--- app/js/components/home.jsx
+++ app/js/components/home.jsx
@@ -9,7 +9,10 @@ module.exports = React.createClass({
   displayName: 'Home Page',
 
   getInitialState() {
-    return {products: []};
+    return {
+      products: [],
+      sort: "name"
+    };
   },
 
   onValueChange: null,
@@ -27,27 +30,51 @@ module.exports = React.createClass({
     ref.child("products").off("value", this.onValueChange);
   },
 
+  changeSort(sort) {
+    this.setState({
+      sort: sort
+    });
+  },
+
   render() {
-    var products = _.compact(this.state.products.map( (product) => {
-      if (product.status !== 'CURRENT') return;
-      return (
-        <tr className="pr" key={product.name}>
-          <th>{product.name}</th>
-          <td>{num(product.qty).format('0,0')+' ft²'}</td>
-        </tr>
-      );
-    }));
+    var products = _(this.state.products)
+      .sortBy(this.state.sort)
+      .map( (product) => {
+        if (product.status !== 'CURRENT') return;
+        return (
+          <tr className="pr" key={product.name}>
+            <th>{product.name}</th>
+            <td>{product.id}</td>
+            <td>{num(product.qty).format('0,0')+' ft²'}</td>
+          </tr>
+        );
+      })
+      .compact()
+      .value();
 
     return (
       <div className="page home-page">
         <h2>Inventory Overview</h2>
         {products.length === 0 && (<p>Loading inventory data...</p>)}
         {products.length > 0 && (
-          <table>
+          <table className="sortable">
             <thead>
               <tr>
-                <th>Name</th>
-                <th>Sq.Ft.</th>
+                <th
+                  className={this.state.sort === 'name' ? 'active' : ''}
+                  onClick={this.changeSort.bind(this, 'name')}>
+                  Name
+                </th>
+                <th
+                  className={this.state.sort === 'id' ? 'active' : ''}
+                  onClick={this.changeSort.bind(this, 'id')}>
+                  Product Code
+                </th>
+                <th
+                  className={this.state.sort === 'qty' ? 'active' : ''}
+                  onClick={this.changeSort.bind(this, 'qty')}>
+                  Sq.Ft.
+                </th>
               </tr>
             </thead>
             <tbody>
